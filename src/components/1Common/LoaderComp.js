3872f7add00880002b26c8fb8d8161e4819345c4
RAJPUROHIT YOGESH SINGH <67944765+rajpurohityogesh@users.noreply.github.com>
2021-09-06 19:28:50 +0530

    Update LoaderComp.js

--- src/components/1Common/LoaderComp.js
+++ src/components/1Common/LoaderComp.js
@@ -4,11 +4,12 @@ import "./Loader.css";
 const LoaderComp = () => {
     return (
         <div className="loader">
-            Loading
+            {
             <div className="one"></div>
             <div className="two"></div>
             <div className="three"></div>
             <div className="four"></div>
+            }
         </div>
     )
 }
